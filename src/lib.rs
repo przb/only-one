b40aa95aeff88ea8eb59af191d1a8450a5253d17
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2025-10-24 17:55:31 -0500

    almost got it

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,26 @@
 // #![no_std]
 
-pub trait OnlyOne {
-    type OnlyOk;
-    type OnlyError;
+use std::io::Read;
 
-    fn only<E, F>(self, f: F) -> Result<Self::OnlyOk, Self::OnlyError>
+pub trait OnlyOne<T> {
+    type Error;
+    fn only<U, E, F>(self, f: F) -> Result<U, Self::Error>
     where
-        E: Into<Self::OnlyError>,
-        F: FnOnce(Self::OnlyOk) -> Result<Self::OnlyOk, Self::OnlyError>,
+        E: Into<Self::Error>,
+        F: FnOnce(T) -> Result<U, Self::Error>,
         Self: Sized;
+    // fn finally<U>(self) -> Result<U, Self::Error>
+    // where
+    //     T: Into<U>;
 }
 
-impl<T, E> OnlyOne for Result<T, E> {
-    type OnlyOk = T;
+impl<Good, Bad> OnlyOne<Good> for Result<Good, Bad> {
+    type Error = Bad;
 
-    type OnlyError = E;
-
-    fn only<I, F>(self, f: F) -> Result<Self::OnlyOk, Self::OnlyError>
+    fn only<U, E, F>(self, f: F) -> Result<U, Self::Error>
     where
-        I: Into<Self::OnlyError>,
-        F: FnOnce(Self::OnlyOk) -> Result<Self::OnlyOk, Self::OnlyError>,
+        E: Into<Self::Error>,
+        F: FnOnce(Good) -> Result<U, Self::Error>,
         Self: Sized,
     {
         match self {
@@ -27,42 +28,86 @@ impl<T, E> OnlyOne for Result<T, E> {
             Err(e) => Err(e.into()),
         }
     }
+    // fn finally<U>(self) -> Result<U, Self::Error>
+    // where
+    //     Good: Into<U>,
+    // {
+    //     match self {
+    //         Ok(v) => Ok(v.into()),
+    //         Err(e) => Err(e.into()),
+    //     }
+    // }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::OnlyOne;
-
-    #[test]
-    fn idk() {
-        #[derive(Debug, thiserror::Error)]
-        #[error("Idk Man")]
-        struct IdkError;
-
-        impl From<std::io::Error> for IdkError {
-            fn from(_io_err: std::io::Error) -> Self {
-                IdkError
-            }
+pub fn foo() -> Result<usize, IdkError> {
+    let f = std::fs::File::open("b");
+    if let Ok(mut f) = f {
+        let mut buf = vec![];
+        let f2 = f.read_vectored(&mut buf);
+        if let Ok(size) = f2 {
+            let array = buf.get(0..size).unwrap();
+            Ok(array.iter().count())
+        } else {
+            Err(IdkError)
         }
-
-        fn idk_dude() -> Result<u32, IdkError> {
-            Ok(0)
-        }
-
-        fn idk_again(_x: String) -> Result<u32, IdkError> {
-            Ok(0)
-        }
-
-        let x = idk_dude();
-        let x = x.and_then(|val| {
-            if val > 3 {
-                let s = std::fs::read_to_string("file");
-                let x = s.only(|v| idk_again(v));
-                // x.into()
-                Ok(())
-            } else {
-                Err(IdkError)
-            }
-        });
+    } else {
+        Err(IdkError)
     }
 }
+
+pub fn fooq() -> Result<usize, IdkError> {
+    let mut f = std::fs::File::open("b")?;
+
+    let mut buf = vec![];
+    let size = f.read_vectored(&mut buf)?;
+
+    let array = buf.get(0..size).unwrap();
+
+    Ok(array.iter().count())
+}
+
+pub fn foo_o() -> Result<usize, IdkError> {
+    let mut buf = vec![];
+    let x = std::fs::File::open("b")
+        .only(|mut f| f.read_vectored(&mut buf))
+        .only(|size| {
+            let array = buf.get(0..size).unwrap();
+            Ok(array.iter().count())
+        })
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("Idk Man")]
+pub struct IdkError;
+
+impl From<std::io::Error> for IdkError {
+    fn from(_io_err: std::io::Error) -> Self {
+        IdkError
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::IdkError;
+//     use crate::OnlyOne;
+
+//     #[test]
+//     fn idk() {
+//         fn idk_dude(_x: u32) -> Result<u32, IdkError> {
+//             Ok(0)
+//         }
+
+//         fn idk_again(_x: String) -> Result<u32, IdkError> {
+//             let val = 4;
+//             if val > 3 {
+//                 let s = std::fs::read_to_string("file");
+//                 s.only(|v| idk_again(v))
+//             } else {
+//                 Err(IdkError)
+//             }
+//         }
+
+//         // let x = 4;
+//         // if x > 4 { Ok(()) } else { Err(IdkError) }
+//     }
+// }
